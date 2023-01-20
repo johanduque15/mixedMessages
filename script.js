17796ca2ff5d7187c7415c9bdea4754e2be942c1
johanduque15 <johan.duque15@gmail.com>
2023-01-20 10:23:22 +0100

    add randomFunction

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 const words1 = ['Study','Work','Focus'];
 const words2 = ['more','and','with'];
 const words3 = ['trust','effort','discipline'];
+
+// creating the random function
+
+const randomFunction = number => {
+    let randomNumber ;
+    randomNumber = Math.floor(Math.random()*number);
+    return randomNumber;
+}
+
+console.log(randomFunction(words1.length));
+console.log(words1[randomFunction(words1.length)]);
